4b4e9c876acefd6803a4912b4c2472a359cc795e
cdwilkie <cdwilkie@aggies.ncat.edu>
2024-05-10 09:11:59 -0400

    Updated HelloJava2.java
    Added JFrame creation to main.

--- HelloJava2.java
+++ HelloJava2.java
@@ -5,13 +5,16 @@ import javax.swing.*;
 public class HelloJava2 {
 
     public static void main(String[] args) {
-        //Create JFrame
-        //Add HelloCOmponent2 to JFrame
-        //show JFrame
-    }
+        JFrame frame = new JFrame ("hellojava2");
+        frame.add(new HelloComponent2("Hello, Java!"));
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(300,300);
+        frame.setVisible(true);
+        
+    }// end main
 }
 
-class HelloComponent2 extends JFrame
+class HelloComponent2 extends JComponent
     implements MouseMotionListener {
 
     String message;
@@ -34,7 +37,7 @@ class HelloComponent2 extends JFrame
     }
 
     public void mouseMoved(MouseEvent e) {
-        
+
     }
 
 
