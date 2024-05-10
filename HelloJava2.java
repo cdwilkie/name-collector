6dd93400cb434c1e36a33dcdc09f86edc633387f
cdwilkie <cdwilkie@aggies.ncat.edu>
2024-05-10 11:22:43 -0400

    Update HelloJava2.java
    Added java doc comments

--- HelloJava2.java
+++ HelloJava2.java
@@ -2,8 +2,18 @@ import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 
+/**
+ * Creates and makes visible a JFrame window.
+ * 
+ */
 public class HelloJava2 {
 
+/**
+ * Creates the JFrame titled 'hellojava2' and adds the
+ * instantiated HelloComponent2 to the frame.  Frame is set to 
+ * exit on close and has default dimensions 300x300.
+ * @param args holds command-line arguments
+ */
     public static void main(String[] args) {
         JFrame frame = new JFrame ("hellojava2");
         frame.add(new HelloComponent2("Hello, Java!"));
@@ -14,21 +24,35 @@ public class HelloJava2 {
     }// end main
 }
 
+/**
+ * Creates a mouse-draggable string component.
+ */
 class HelloComponent2 extends JComponent
     implements MouseMotionListener {
 
     String message;
     int messageX = 125, messageY = 95;
 
+ /**
+  * Constructor accepts string for message.
+  * @param message stores message to be used for interactive text
+  */   
     public HelloComponent2(String message) {
         this.message = message;
         addMouseMotionListener(this);
     }
-
+/**
+ * Override that draws the message at the x and y coordinates
+ * @param g Graphics area for drawing
+ */
     public void paintComponent(Graphics g) {
         g.drawString(message, messageX, messageY);
     }
 
+/**
+ * Collects the x and y coordinate of the mouse cursor when dragged
+ * then updates the image by repainting
+ */
     public void mouseDragged(MouseEvent e) {
         messageX = e.getX();
         messageY = e.getY();
@@ -36,6 +60,10 @@ class HelloComponent2 extends JComponent
         repaint();
     }
 
+/**
+ * Not currently implemented. Will detail behavior
+ * for detected mouse movement.
+ */    
     public void mouseMoved(MouseEvent e) {
 
     }
