db34390c07163544c0f444a787a50c5a1ae788d7
cdwilkie <cdwilkie@aggies.ncat.edu>
2024-05-10 02:51:26 -0400

    HelloJava demo
    JFrame Demo. Also explore invokeLater with Swing

---
+++ HelloJava.java
@@ -0,0 +1,35 @@
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class HelloJava{
+    public static void main(String[] args) {
+
+    
+        JFrame helloFrame = new JFrame("Hello, Java!");
+        helloFrame.setSize(300, 300);
+        helloFrame.setVisible(true);
+   
+
+    /*
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                createAndShowGUI();
+            }
+        });
+    */
+    }
+
+    private static void createAndShowGUI() {
+        JFrame frame = new JFrame("Frame Demo");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        JLabel emptyLabel = new JLabel("");
+        emptyLabel.setPreferredSize(new Dimension(175, 100));
+        frame.getContentPane().add(emptyLabel, BorderLayout.CENTER);
+
+        frame.pack();
+        frame.setVisible(true);
+    }
+}
