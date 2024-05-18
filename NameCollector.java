a98abec5b3aa48ec910476bd9b911996ac0b059c
cdwilkie <cdwilkie@aggies.ncat.edu>
2024-05-18 05:53:14 -0400

    Update NameCollector.java
    Added Comments

--- NameCollector.java
+++ NameCollector.java
@@ -13,79 +13,86 @@ public class NameCollector {
    private int userAge;
    private double userInches;
 
+   /**
+    * Constructor initializes all variables with values
+    */
    public NameCollector() {
       setPrompt();
       userName = "";
       userAge = 0;
       userInches = 0.0;
-   }
+   }//end Constructor
 
-   public void setPrompt() {
+   private void setPrompt() {
       thePrompt = "Welcome!\nPlease enter your: <Name>, <Age>, and <Height-Inches>";
-   }
+   }//end setPrompt()
    
    public String getPrompt() {
       return thePrompt;
-   }
+   }//end getPrompt()
 
    public void showPrompt() {
       System.out.println(thePrompt);
-   }
+   }//end showPrompt()
 
    public void setUserName(String inputName) {
       userName = inputName;
-   }
+   }//end setUserName()
 
    public String getUsername() {
       return userName;
-   }
+   }//end getUserName
 
    public void setAge(int inputAge) {
       userAge = inputAge;
-   }
+   }//end setAge()
 
    public int getAge() {
       return userAge;
-   }
+   }// end getAge()
 
    public void setInches(double inputInches) {
       userInches = inputInches;
-   }
+   }//end setInches()
 
    public double getInches() {
       return userInches;
-   }
+   }//end getInches()
 
    public void setOutputMessage() {
       outputMessage = "You entered:\n\tName:\t" + userName;
       outputMessage += "\n\tAge:\t" + userAge;
       outputMessage += "\n\tInches:\t" + userInches;
-   }
+   }//end setOuputMessage
 
    public String getOutputMessage() {
       return outputMessage;
-   }
+   }//end getOutputMessage
    
+/**
+ * Constructs an instance of NameCollector to store
+ * the user provided <Name>, <Age>, and <Inches>. Attempts
+ * to collect the information from System.in and display
+ * the data to console.
+ * @param args command line input - not used
+ */
    public static void main(String[] args) {
       //Scanner scanner = new Scanner(System.in);
       NameCollector newDemo = new NameCollector();
-      newDemo.showPrompt();
+      newDemo.showPrompt();                     // Print prompt to console
 
       try (Scanner scanner = new Scanner(System.in)) 
       {
-         newDemo.setUserName(scanner.next());
-         newDemo.setAge(scanner.nextInt());
-         newDemo.setInches(scanner.nextDouble());
-         newDemo.setOutputMessage();
-         System.out.println(newDemo.getOutputMessage());
-      }
+         newDemo.setUserName(scanner.next());   // Store username from input
+         newDemo.setAge(scanner.nextInt());     // Store userage from input
+         newDemo.setInches(scanner.nextDouble());// Store userInches from input
+         newDemo.setOutputMessage();            // Update the output message
+         System.out.println(newDemo.getOutputMessage());//Display data to console
+      }//end try
       catch (Exception e) {
          System.out.println("Something wrong occured");
-      }
+      }//end catch
+      
+   }//end main()
 
-      
-      
-      
-   }
-
-}
+}//end NameCollector
