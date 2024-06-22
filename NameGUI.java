27addce1ccdab9e2bcaa383f760216466ff5c299
cdwilkie <cdwilkie@aggies.ncat.edu>
2024-06-22 02:13:03 -0400

    Implemented addNameComponents()
    Updated NameGUI.java by implementing addNameComponents() method. Creates a JLabel and JTextField and Adds to JPanel.

--- NameGUI.java
+++ NameGUI.java
@@ -1,7 +1,7 @@
 import java.awt.*;
-import java.awt.event.*;
+//import java.awt.event.*;
 import javax.swing.*;
-import java.text.*;
+//import java.text.*;
 import javax.swing.border.*;
 
 
@@ -25,17 +25,46 @@ public class NameGUI {
         });//end invokeLater()
     }//end main()
 
+    /**
+     * Creates the necessary <code>JComponents</code> to
+     * collect text input for a "name" prompt. Utilizes a
+     * <code>GridBagLayout</code> to organize the elements.
+     * @param targetPanel <code>JPanel</code> to hold the
+     * arranged <code>JComponents</code>
+     */
     private static void addNameComponents(JPanel targetPanel) {
-        //TODO
         //create label
+        JLabel nameLabel = new JLabel("Name: ");
         //create field
-        //create constraints
-        //format label constraints
-        //add label to target jpanel with constrints
-        //format field constraints
-        //add field to target jpanel with constraints
+        JTextField nameField = new JTextField("Enter Name");
+        nameField.setColumns(15);
+        nameField.setEditable(true);
 
-    }
+        //create constraints
+        GridBagConstraints layoutConstraints = new GridBagConstraints();
+
+        //format label constraints
+        layoutConstraints.gridx = 0;
+        layoutConstraints.gridy = 0;
+        layoutConstraints.fill = GridBagConstraints.BOTH;
+        layoutConstraints.weightx = 1.0;
+        layoutConstraints.weighty = 1.0;
+        layoutConstraints.insets = new Insets(10, 10, 10, 10);
+
+        //add label to target jpanel with constrints
+        targetPanel.add(nameLabel, layoutConstraints);
+
+        //format field constraints
+        layoutConstraints.gridx = 1;
+        layoutConstraints.gridy = 0;
+        layoutConstraints.fill = GridBagConstraints.BOTH;
+        layoutConstraints.weightx = 1.0;
+        layoutConstraints.weighty = 1.0;
+        layoutConstraints.insets = new Insets(10, 10, 10, 10);
+
+        //add field to target jpanel with constraints
+        targetPanel.add(nameField, layoutConstraints);
+    }//end addNameComponents()
 
     /**
      * Adds the target <code>JPanel</code> to the target <code>
@@ -139,13 +168,15 @@ public class NameGUI {
         GridBagConstraints layoutConstraints = new GridBagConstraints();
 
         // format panel constraints
-        layoutConstraints.gridx = 0;
+        layoutConstraints.gridx = 2;
         layoutConstraints.gridy = 3;
         layoutConstraints.fill = GridBagConstraints.BOTH;
         layoutConstraints.anchor = GridBagConstraints.BASELINE;
         layoutConstraints.weightx = 1.0;
         layoutConstraints.weighty = 1.0;
         layoutConstraints.insets = new Insets(10, 10, 10, 10);
+        layoutConstraints.gridheight = 1;
+        layoutConstraints.gridwidth = 1;
 
         // add panel to frame with constraints
         targetFrame.add(targetPanel, layoutConstraints);
@@ -161,36 +192,38 @@ public class NameGUI {
     private static void buildFrameUI() {
         // Make Frame
         JFrame mainFrame = new JFrame("The Frame");
+        // Set Layout Manager
+        mainFrame.setLayout(new GridBagLayout());
         // Make jpanel
         JPanel namePanel = new JPanel();
-        // addNameComponents(JPanel)
-        addNameComponents(namePanel);
         // Customize Panel
         customizePanel(namePanel);
+        // addNameComponents(JPanel)
+        addNameComponents(namePanel);
         // addNamePanel(JPanel, JFrame)
         addNamePanel(namePanel, mainFrame);
         // Make jpanel
         JPanel agePanel = new JPanel();
-        // addAgeComponents(jPanel)
-        addAgeComponents(agePanel);
         // Customize Panel
         customizePanel(agePanel);
+        // addAgeComponents(jPanel)
+        addAgeComponents(agePanel);
         // addAgePanel(JPanel, JFrame)
         addAgePanel(agePanel, mainFrame);
         // Make Jpanel
         JPanel inchesPanel = new JPanel();
-        // addInchesComponents(JPanel)
-        addInchesComponents(inchesPanel);
         // Customize Panel
         customizePanel(inchesPanel);
+        // addInchesComponents(JPanel)
+        addInchesComponents(inchesPanel);
         // addInchesPanel(JPanel, JFrame)
         addInchesPanel(inchesPanel, mainFrame);
         // Make JPanel
         JPanel acceptPanel = new JPanel();
-        // addAcceptComponents(JPanel)
-        addAcceptComponents(acceptPanel);
         // Customize Panel
         customizePanel(acceptPanel);
+        // addAcceptComponents(JPanel)
+        addAcceptComponents(acceptPanel);
         // addAcceptPanel(JPanel, JFrame)
         addAcceptPanel(acceptPanel, mainFrame);
         // Customize JFrame
@@ -204,8 +237,7 @@ public class NameGUI {
      * @param targetFrame <code>JFrame</code> that is to be customized
      */
     private static void customizeFrame(JFrame targetFrame) {
-        // Set Layout Manager
-        targetFrame.setLayout(new GridBagLayout());
+        
         // Set Background Color
         targetFrame.getContentPane().setBackground(Color.DARK_GRAY);
         // Display Frame
