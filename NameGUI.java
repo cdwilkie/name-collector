6d13a5301d63320e54e4a785806ffc0431eb9665
cdwilkie <cdwilkie@aggies.ncat.edu>
2024-06-21 21:35:14 -0400

    Added Function Stubs to NameGUI.java
    Updated NameGUI.java with function stubs to implement the GUI

--- NameGUI.java
+++ NameGUI.java
@@ -1,198 +1,126 @@
+import java.awt.*;
+import java.awt.event.*;
 import javax.swing.*;
 import java.text.*;
-import javax.swing.text.*;
-import java.awt.event.*;
-import java.awt.*;
+import javax.swing.border.*;
 
-public class NameGUI extends JFrame implements ActionListener {
+
+/**
+ * Serves as the GUI for the Name Collector Program
+ */
+public class NameGUI {
+
     public static void main(String[] args) {
-        /* 
-        NameGUI newDemo = new NameGUI();
-        newDemo.pack();
-        newDemo.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        newDemo.setVisible(true);
-        */
-
-        NameFrame newDemo = new NameFrame();
-        newDemo.setVisible(true);
+        buildFrameUI();
     }
 
-    private JLabel nameLabel, ageLabel, inchesLabel;
-    private JTextField nameField;
-    private JFormattedTextField ageField, inchesField;
-    private JButton acceptButton;
-
-    public NameGUI () {
-        nameLabel = new JLabel("Name: ");
-        ageLabel = new JLabel("Age: ");
-        inchesLabel = new JLabel("Height in inches: ");
-
-        nameField = new JTextField(20);
-        nameField.setEditable(true);
-        nameField.setText("John Doe");
-
-        ageField = new JFormattedTextField(NumberFormat.getIntegerInstance());
-        ageField.setColumns(15);
-        ageField.setEditable(true);
-        ageField.setValue(69);
-
-        inchesField = new JFormattedTextField(NumberFormat.getInstance());
-        inchesField.setColumns(15);
-        inchesField.setEditable(true);
-        inchesField.setValue(42.0);
-
-        acceptButton = new JButton("Accept");
-        acceptButton.addActionListener(this);
-
-        setLayout(new GridBagLayout());
-
-        GridBagConstraints layoutConsts = new GridBagConstraints();
-        layoutConsts.gridx = 0;
-        layoutConsts.gridy = 0;
-        layoutConsts.insets = new Insets(10,10,10,10);
-        add(nameLabel, layoutConsts);
-
-        layoutConsts = new GridBagConstraints();
-        layoutConsts.gridx = 1;
-        layoutConsts.gridy = 0;
-        layoutConsts.insets = new Insets(10,10,10,10);
-        add(nameField, layoutConsts);
-
-        layoutConsts = new GridBagConstraints();
-        layoutConsts.gridx = 0;
-        layoutConsts.gridy = 1;
-        layoutConsts.insets = new Insets(10,10,10,10);
-        add(ageLabel, layoutConsts);
-
-        layoutConsts = new GridBagConstraints();
-        layoutConsts.gridx = 1;
-        layoutConsts.gridy = 1;
-        layoutConsts.insets = new Insets(10,10,10,10);
-        add(ageField, layoutConsts);
-
-        layoutConsts = new GridBagConstraints();
-        layoutConsts.gridx = 0;
-        layoutConsts.gridy = 2;
-        layoutConsts.insets = new Insets(10,10,10,10);
-        add(inchesLabel, layoutConsts);
-
-        layoutConsts = new GridBagConstraints();
-        layoutConsts.gridx = 1;
-        layoutConsts.gridy = 2;
-        layoutConsts.insets = new Insets(10,10,10,10);
-        add(inchesField, layoutConsts);
-
-        layoutConsts = new GridBagConstraints();
-        layoutConsts.gridx = 2;
-        layoutConsts.gridy = 3;
-        layoutConsts.insets = new Insets(10,10,10,10);
-        add(acceptButton, layoutConsts);
+    private static void addNameComponents(JPanel targetPanel) {
+        //create label
+        //create field
+        //create constraints
+        //format label constraints
+        //add label to target jpanel with constrints
+        //format field constraints
+        //add field to target jpanel with constraints
 
     }
 
+    private static void addNamePanel(JPanel targetPanel, JFrame targetFrame) {
+        // Create constraints
+        // format panel constraints
+        // add panel to frame with constraints
+    }
 
+    private static void addAgeComponents(JPanel targetPanel) {
 
-    @Override
-    public void actionPerformed(ActionEvent e) {
-        JOptionPane.showMessageDialog(this, "Yuo have entered data and preesed accept");
+    }
+
+    private static void addAgePanel(JPanel targetPanel, JFrame targetFrame) {
+
+    }
+
+    private static void addInchesComponents(JPanel targetPanel) {
+
+    }
+
+    private static void addInchesPanel(JPanel targetPanel, JFrame targetFrame) {
+
+    }
+
+    private static void addAcceptComponents(JPanel targetPanel) {
+
+    }
+
+    private static void addAcceptPanel(JPanel targetPanel, JFrame targetFrame) {
+
     }
     
-}
+    private static void buildFrameUI() {
+        // Make Frame
+        JFrame mainFrame = new JFrame("The Frame");
+        // Make jpanel
+        JPanel namePanel = new JPanel();
+        // addNameComponents(JPanel)
+        addNameComponents(namePanel);
+        // Customize Panel
+        customizePanel(namePanel);
+        // addNamePanel(JPanel, JFrame)
+        addNamePanel(namePanel, mainFrame);
+        // Make jpanel
+        JPanel agePanel = new JPanel();
+        // addAgeComponents(jPanel)
+        addAgeComponents(agePanel);
+        // Customize Panel
+        customizePanel(agePanel);
+        // addAgePanel(JPanel, JFrame)
+        addAgePanel(agePanel, mainFrame);
+        // Make Jpanel
+        JPanel inchesPanel = new JPanel();
+        // addInchesComponents(JPanel)
+        addInchesComponents(inchesPanel);
+        // Customize Panel
+        customizePanel(inchesPanel);
+        // addInchesPanel(JPanel, JFrame)
+        addInchesPanel(inchesPanel, mainFrame);
+        // Make JPanel
+        JPanel acceptPanel = new JPanel();
+        // addAcceptComponents(JPanel)
+        addAcceptComponents(acceptPanel);
+        // Customize Panel
+        customizePanel(acceptPanel);
+        // addAcceptPanel(JPanel, JFrame)
+        addAcceptPanel(acceptPanel, mainFrame);
+        // Customize JFrame
+        customizeFrame(mainFrame);
+       
+    }//end buildFrameUI()
 
-class NameFrame extends JFrame {
-    private NameComponent nameComponent;
-    private AgeComponent ageComponent;
-    private InchesComponent inchesComponent;
-    private JButton acceptButton;
-    private GridBagConstraints layoutConst;
-
-
-    NameFrame() {
-        nameComponent = new NameComponent();
-        ageComponent = new AgeComponent();
-        inchesComponent = new InchesComponent();
-        acceptButton = new JButton("Accept");
-
-        setLayout(new GridBagLayout());
-        layoutConst = new GridBagConstraints();
-
-        layoutConst.gridx = 0;
-        layoutConst.insets = new Insets(10, 10, 10, 10);
-        
-        add(nameComponent,layoutConst);
-
-        layoutConst.gridx = 1;
-        add(ageComponent, layoutConst);
-
-        layoutConst.gridx = 2;
-        add(inchesComponent, layoutConst);
-
-        layoutConst.gridx = 3;
-        layoutConst.gridy = 2;
-        add(acceptButton, layoutConst);
-
-        //setVisible(true);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        pack();
-    }
-}
-
-class NameComponent extends JComponent{
-    private JPanel namePanel;
-    private JLabel nameLabel;
-    private JTextField nameField;
-
-    NameComponent() {
-        nameLabel = new JLabel("Name: ");
-        nameField = new JTextField(15);
-        nameField.setText("Enter Name Here");
-        namePanel = new JPanel();
-        buildPanel();
-    }
-
-    private void buildPanel() {
-        GridBagConstraints layoutConst = new GridBagConstraints();
-        namePanel.setLayout(new GridBagLayout());
-
-        layoutConst.gridx = 0;
-        layoutConst.gridy = 0;
-        layoutConst.insets = new Insets(10, 10, 10, 10);
-        namePanel.add(nameLabel, layoutConst);
-
-        layoutConst.gridx = 0;
-        layoutConst.gridy = 1;
-        namePanel.add(nameField, layoutConst);
-
-    }
-}
-
-class AgeComponent extends JComponent {
-    private JLabel ageLabel;
-    private JFormattedTextField ageField;
-    private JPanel agePanel;
-
-    AgeComponent() {
-        ageLabel = new JLabel("Age: ");
-        ageField = new JFormattedTextField(NumberFormat.getIntegerInstance());
-        agePanel = new JPanel();
-        agePanel.add(ageLabel);
-        agePanel.add(ageField);
+    private static void customizeFrame(JFrame targetFrame) {
+        // Set Layout Manager
+        targetFrame.setLayout(new GridBagLayout());
+        // Set Background Color
+        targetFrame.getContentPane().setBackground(Color.DARK_GRAY);
+        // Display Frame
+        targetFrame.setVisible(true);
+        // Close Operation
+        targetFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        // pack()
+        targetFrame.pack();
+        // minimum size
+        targetFrame.setMinimumSize(new Dimension(450, 300));
     }
 
-}
-
-class InchesComponent extends JComponent {
-    private JLabel inchesLabel;
-    private JFormattedTextField inchesField;
-    private JPanel inchesPanel;
-
-    InchesComponent() {
-        inchesLabel = new JLabel("Inches Tall: ");
-        inchesField = new JFormattedTextField(DecimalFormat.getInstance());
-        inchesPanel = new JPanel();
-        inchesPanel.add(inchesLabel);
-        inchesPanel.add(inchesField);
+    private static void customizePanel(JPanel targetPanel) {
+        // set layout manager
+        targetPanel.setLayout(new GridBagLayout());
+        // Set panel background color to white
+        targetPanel.setBackground(Color.WHITE);
+        // apply border (compound black soft bevel)
+        Border blackBorder = BorderFactory.createLineBorder(Color.BLACK);
+        Border softBevelBorder = BorderFactory.createRaisedSoftBevelBorder();
+        targetPanel.setBorder(BorderFactory.createCompoundBorder(softBevelBorder, blackBorder));
     }
+
+    
+
 }
-
-
